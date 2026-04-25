0acac84fcf94aeb68eea0d9d9dc9ef2bdb5c8938
mardbrito <marcelo4web@gmail.com>
2026-04-25 09:39:38 +0800

    scaffold better-auth and drizzle

---
+++ app/lib/auth.ts
@@ -0,0 +1,23 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/lib/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    usePlural: true,
+  }),
+
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
+
+  advanced: {
+    database: {
+      generateId: false,
+    },
+  },
+});
---
+++ app/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  I am main index page
+</template>
